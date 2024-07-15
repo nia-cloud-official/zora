6e9d3933ea8d00ff5506fd5e4ef7ff4594c7accc
nia-cloud-official <niacloudofficial@gmail.com>
2024-07-15 20:14:18 +0100

    renamed:    Views/dashboard/ cvbmn,.sql -> Views/dashboard/cvbmn,.sql 	modified:   core/applications_5/applications_5.zoraConfig

--- core/core.php
+++ core/core.php
@@ -1,1 +1,86 @@
-<?php 
+<?php
+
+define('__DIR__', 'http://localhost/zora/');
+
+define('__APPNAME__', 'Zora Lumin');
+
+define('home', 'index.php');
+
+class Process
+{
+    private $dummy = "public/"; # this main folder holding all the view;
+    function __construct()
+    {
+        $dummy = $this->dummy;
+        //$this->__loadMainView($dummy . home);
+    }
+    function __loadMainView($location)
+    {
+        header('Location: ' . $location);
+    }
+    function checkCredentials()
+    {
+        if (empty($host) || empty($user) || empty($pass)) {
+            echo
+                "
+                <center>
+                <div class='content-wrapper'>
+                <div class='col-lg-4 flex-margin'>
+                <div class='card'>
+                <div class='card-body'>
+                 <div class='row'>
+                    <h4 class='card-title'>Zora Connection Setup</h4>
+                    <p class='card-description'>To get started with Zora please establish a connection first!</p>
+                    <form class='forms-sample' method='post'action=''>
+                      <div class='form-group'>
+                        <label for='exampleInputUsername1'>Host</label>
+                        <input type='text' class='form-control' id='exampleInputUsername1' name='host' placeholder='Host'>
+                      </div>
+                      <div class='form-group'>
+                        <label for='exampleInputEmail1'>Username</label>
+                        <input type='text' class='form-control' name='username' id='exampleInputEmail1' placeholder='Username'>
+                      </div>
+                      <div class='form-group'>
+                        <label for='exampleInputPassword1'>Password</label>
+                        <input type='password' class='form-control' name='password' id='exampleInputPassword1' placeholder='Password'>
+                      </div>
+                      <button type='submit' class='btn btn-gradient-primary me-2'>Connect</button>
+                    </form>
+                  </div>
+                  </div>
+              </div>
+                </div>
+                </div>
+                </center>
+            ";
+        } else {
+            echo "Your credentials have been set.";
+        }
+    }
+    function startApp()
+    {
+        $this->checkCredentials();
+    }
+
+    function establishConnection($host,$username,$password){ 
+        $userConn = mysqli_connect($host,$username,$password);
+        if(!mysqli_connect_error()){
+            echo "Successfully established connection";
+            header('location: public/index.view.php');
+        }else{
+            echo "Unable to establish connection";
+        }
+    }
+}
+
+$action = new Process();
+if(isset($_POST['host'])){ 
+    $host = $_POST['host'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $action->establishConnection($host,$username,$password);
+    header('location: public/index.view.php');
+}else{ 
+    echo "Your credentials have not been set.";
+}
+?>
--- index.php
+++ index.php
@@ -1,3 +1,36 @@
 <?php
-header("Location: ./Views/dashboard/index.php")
+
+# [ Core Folder ]
+include 'core/core.php';
+
+$zora = new Process();
+$zora->startApp();
 ?>
+
+<head>
+   <link rel='stylesheet' href='./assets/vendors/mdi/css/materialdesignicons.min.css'>
+   <link rel='stylesheet' href='./assets/vendors/css/vendor.bundle.base.css'>
+   <link rel='stylesheet' href='./assets/css/style.css'>
+   <link rel='stylesheet' href='./assets/css/style.css'>
+   <link rel='shortcut icon' href='../assets/images/favicon.ico'/>
+   <script src='./assets/vendors/js/vendor.bundle.base.js'></script>
+   <script src='./assets/js/off-canvas.js'></script>
+   <script src='./assets/js/hoverable-collapse.js'></script>
+   <script src='./assets/js/misc.js'></script>
+   <link rel='stylesheet' href='./../assets/vendors/mdi/css/materialdesignicons.min.css'>
+   <link rel='stylesheet' href='./../assets/vendors/css/vendor.bundle.base.css'>
+
+
+   <link rel='stylesheet' href='./../assets/css/style.css'>
+
+   <link rel='shortcut icon' href='./../assets/images/favicon.ico'/>
+   <script src='./../assets/vendors/js/vendor.bundle.base.js'></script>
+
+
+   <script src='./../assets/js/off-canvas.js'></script>
+   <script src='./../assets/js/hoverable-collapse.js'></script>
+   <script src='./../assets/js/misc.js'></script>
+
+
+   <script src='./../assets/js/file-upload.js'></script>
+</head>
